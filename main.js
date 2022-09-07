a8af59ecac16c7156b247ec3a3c71e84de285998
TiaanSM <108478468+TiaanSM@users.noreply.github.com>
2022-09-07 13:06:31 +0200

    Vue js computed properties && components

--- main.js
+++ main.js
@@ -2,22 +2,10 @@ const app = Vue.createApp({
     data: function() {
         return {
             cart: 0,
-            product: 'Socks',
-            image: './assets/images/socks_green.jpg',
-            inventory: 20,
-            details: ['50% cotton', '30% wool', '20% polyetster'],
-            variants: [
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg'},
-                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg'},
-            ]
+            premium: true
         }
     },
     methods: {
-        addToCart() {
-            this.cart += 1
-        },
-        updateImage(variantImage) {
-            this.image = variantImage
-        }
+      
     }
 })
